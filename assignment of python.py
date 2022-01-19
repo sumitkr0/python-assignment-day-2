1b0d17ad1d09791c59bc618e3d801ac17c9c5e28
sumitkr0 <97955617+sumitkr0@users.noreply.github.com>
2022-01-19 14:23:05 +0530

    assignment submitted

---
+++ assignment of python.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#datatypes
+
+
+# In[6]:
+
+
+##float
+
+
+# In[7]:
+
+
+10/2
+
+
+# In[8]:
+
+
+a=5
+
+
+# In[9]:
+
+
+b=2
+
+
+# In[12]:
+
+
+c=a/b
+
+
+# In[13]:
+
+
+c
+
+
+# In[14]:
+
+
+type(c)
+
+
+# In[15]:
+
+
+##int
+
+
+# In[16]:
+
+
+marks=100
+
+
+# In[17]:
+
+
+type(marks)
+
+
+# In[18]:
+
+
+##string
+
+
+# In[19]:
+
+
+name='assignment work'
+
+
+# In[20]:
+
+
+type(name)
+
+
+# In[21]:
+
+
+##bool
+
+
+# In[22]:
+
+
+isRight=True
+isWrong=False
+
+
+# In[23]:
+
+
+type(isRight)
+
+
+# In[24]:
+
+
+type(isWrong)
+
+
+# In[25]:
+
+
+##print
+
+
+# In[27]:
+
+
+print("assignment")
+
+
+# In[28]:
+
+
+name='assignment'
+
+
+# In[29]:
+
+
+print(name)
+
+
+# In[32]:
+
+
+print ('python  '   +name)
+
+
+# In[38]:
+
+
+time='2 hours'
+
+
+# In[34]:
+
+
+place='jaipur'
+
+
+# In[39]:
+
+
+print(name,time,place)
+
+
+# In[36]:
+
+
+##fstring
+
+
+# In[51]:
+
+
+print(f'my work is {name} and time taken {time} and doing at {place}')
+
+
+# In[52]:
+
+
+##list
+
+
+# In[53]:
+
+
+total_marks = list()
+
+
+# In[54]:
+
+
+total_marks.append(34)
+
+
+# In[55]:
+
+
+total_marks.append(87.5)
+
+
+# In[56]:
+
+
+total_marks.append('assignment')
+
+
+# In[57]:
+
+
+total_marks.append([12,13,14,23])
+
+
+# In[58]:
+
+
+total_marks
+
+
+# In[59]:
+
+
+##tuple
+
+
+# In[60]:
+
+
+tuple_a=tuple()
+
+
+# In[61]:
+
+
+type(tuple_a)
+
+
+# In[62]:
+
+
+tuple_a = (1,2,3,4)
+
+
+# In[63]:
+
+
+tuple_b=(tuple_a)
+
+
+# In[64]:
+
+
+tuple_b
+
+
+# In[65]:
+
+
+min(tuple_a)
+
+
+# In[66]:
+
+
+len(tuple_a)
+
+
+# In[69]:
+
+
+tuple_a.index(3)
+
+
+# In[70]:
+
+
+##dict
+
+
+# In[71]:
+
+
+{
+    'work': 'assignment',
+    'language': 'python',
+    
+    
+}
+
+
+# In[ ]:
+
+
+
+
